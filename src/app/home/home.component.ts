7bbcabf80b32972b367f51f518fac236c9477ab1
Gillich Norbert <gillich.norbert@icloud.com>
2024-11-04 15:32:10 +0100

    Home kezdet

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BaseService } from '../base.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  magvak:any=[]
+  columns=[
+    {key:"name", text:"Név", type:'text'},
+    {key:"price", text:"Ár", type:'number'},
+    {key:"description", text:"Leírás", type:'textarea'},
+    {key:"picture", text:"Kép", type:"textarea"}
+  ]
 
+  constructor(private base:BaseService) {
+    this.base.getProducts().snapshotChanges().pipe(
+      map(
+        (changes)=>changes.map(
+          (c)=>({key:c.payload.key, ...c.payload.val()})
+        )
+       )
+    ).subscribe(
+      (res)=>this.magvak=res
+    )
+  }
 }
